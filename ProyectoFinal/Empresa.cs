4316384e8c8ab509804a9d14da971c1aadb32a90
Richard Villalba <richard.villalba.mendieta@gmail.com>
2022-11-25 17:44:44 -0400

    empresa

---
+++ ProyectoFinal/Empresa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ProyectoFinal
+{
+    public class Empresa
+    {
+        //atributos//
+        private string nombreDeLaEmpresa;
+        private int nitDeLaEmpresa;
+
+        //contructores//
+        public Empresa()
+        {
+            this.nombreDeLaEmpresa = "Mi Empresa";
+            this.nitDeLaEmpresa = 1111;
+        }
+
+        //methods//
+        public virtual void VerDatos()
+        {
+            Console.WriteLine($"Empresa: {this.nombreDeLaEmpresa} \t NIT: {this.nitDeLaEmpresa}");
+        }
+
+        public virtual void LeerDatos()
+        {
+            Console.Write($"Ingresa el nombre de la Empresa: ");
+            this.nombreDeLaEmpresa = Console.ReadLine();
+
+            try
+            {
+                Console.Write($"Ingresa el nit de la empresa {this.nombreDeLaEmpresa}: ");
+                this.nitDeLaEmpresa = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ingresaste un nit no valido, por Defecto se usara el nit: 1111111");
+                this.nitDeLaEmpresa = 1111111;
+            }
+        }
+        
+    }
+}
+
